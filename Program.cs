855399e00a5b8fd4cfeab7af609ce6e027398012
ZeroRequiem74 <zerorequiem2474@gmail.com>
2022-10-06 13:13:21 -0600

    changed

--- Program.cs
+++ Program.cs
@@ -5,15 +5,14 @@
         Player player = new Player();
         Game game = new Game();
         int points = 0;
-        while(!IsGameOver(game, player, points))
+        do
         {
             int DrawOne = game.FirstCard();
-            
             string PlayerChoice = player.PlayerChoice();
             int DrawTwo = game.SecondDraw(DrawOne);
             points += game.IsHigher(DrawOne, DrawTwo, PlayerChoice, points);
 
-        }
+        }while(!IsGameOver(game, player, points));
     }
 
     static bool IsGameOver(Game game, Player player, int points)
