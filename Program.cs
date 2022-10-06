6ddc77e560351de3b58338381abcc6bb2593c3fd
Stroboscopic90 <113635128+Strobo90@users.noreply.github.com>
2022-10-06 12:35:36 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,17 +5,10 @@
         Player player = new Player();
         Game game = new Game();
         int points = 0;
-        while(!IsGameOver(game, player))
+       new Game();
+        while(IsGameOver(game))
         {
-            int DrawOne = game.FirstCard();
-            int DrawTwo = game.SecondDraw(DrawOne);
-            string PlayerChoice = player.PlayerChoice();
-            points += game.IsHigher(DrawOne, DrawTwo, PlayerChoice, points);
+            
         }
-    }
-
-    static bool IsGameOver(Game game, Player player)
-    {
-        return false;
     }
 }
