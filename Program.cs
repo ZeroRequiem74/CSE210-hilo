21aa593a6d1073347d57eae9a4e3926a865608ea
Stroboscopic90 <113635128+Strobo90@users.noreply.github.com>
2022-10-08 17:34:30 -0600

    122

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿class HiLo // creates the Game class
 {
-    static void Main(string[] args)
+    static void Main(string[] args) // main function of the Game
     {
         Player player = new Player();
         Game game = new Game();
-        int points = 0;
+        int points = 0;// initialize the points to zero at first
         do
         {
-            int DrawOne = game.FirstCard();
+            int DrawOne = game.FirstCard();// checks the card drawn
             string PlayerChoice = player.PlayerChoice();
             int DrawTwo = game.SecondDraw(DrawOne);
             points += game.IsHigher(DrawOne, DrawTwo, PlayerChoice);
@@ -15,7 +15,7 @@
         }while(!IsGameOver(game, player, points));
     }
 
-    static bool IsGameOver(Game game, Player player, int points)
+    static bool IsGameOver(Game game, Player player, int points)// checks Lifepoints to continue or end game
     {
         if(game.IsGameOver(player.PlayerLifePoints(points)))
         {
